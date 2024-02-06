091539407d1d6f06722400fcd3529022aed108f6
lokesh bajracharya <lokesh.bajracharya55@gmail.com>
2024-02-06 19:30:27 +0545

    convert to 5 rounds lol

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,5 +1,5 @@
 export const INIT_SQUARES = Array(9).fill(null);
 export const STORE_KEY = "rounds";
-export const MAX_ROUNDS = 3;
+export const MAX_ROUNDS = 5;
 export const PLAYERX = "X";
 export const PLAYERO = "O";
